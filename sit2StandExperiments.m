95f1a579c2c50662ee28963d8ae82e579f3a054a
Stephan Stansfield <stephan.stansfield@gmail.com>
2026-06-03 14:41:37 -0400

    update stand-to-sit trajectory to keep cable in groove

--- sit2StandExperiments.m
+++ sit2StandExperiments.m
@@ -43,8 +43,8 @@ t_record = linspace(0, T_tot, N_tot);
 % q = deg2rad([-101.06, 104.70, -0.01, -53.22, -113.81, 81.67, -18.19]'); % table against wall, sit-to-stand groove aligned
 % q = deg2rad([-97.50, 102.43, -0.01, -63.26, -111.18, 85.67, -11.39]'); % table against wall, spring jig clamped to table, sit-to-stand groove aligned
 % q = deg2rad([-97.55, 101.22, 0.39, -65.36, -111.28, 85.98, -10.55]'); % updated 05-21-2026
-q = deg2rad([-98.15, 101.29, 0.39, -64.97, -111.85, 85.72, -10.81]'); % sit-to-stand updated 05-21-2026_02
-% q = deg2rad([-98.14, 102.42, 0.39, -63.52, -111.83, 85.61, -11.09]'); % stand-to-sit updated 05-22-2026
+% q = deg2rad([-98.15, 101.29, 0.39, -64.97, -111.85, 85.72, -10.81]'); % sit-to-stand updated 05-21-2026_02
+q = deg2rad([-97.86, 103.13, 0.39, -62.76, -111.56, 85.66, -11.16]'); % stand-to-sit updated 06-03-26
 
 H0 = robot.getForwardKinematics(q); 
 R0 = H0(1:3, 1:3);                      % Starting orientation    
